7c46c3f57c4f81ea14f31abdf8eb5230e3f79d7a
nariogulu <emuolanari.osah-ogulu@northlink.digital.com>
2022-07-10 10:00:48 +0100

    changed image path

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import WaitingListBtn from '../src/components/WaitingListBtn'
 import GroupGirl from './../src/assets/GroupGirl.png'
 import NigerGirl from './../src/assets/NigerGirl.png'
 import HeadphoneGirl from './../src/assets/headphoneGirl.png'
-import ThumbNailGirl from './../src/assets/thumbNailGirl.png'
+import ThumbNailGirl from './../src/assets/thumbnailgirl.png'
 import MiniCard from '../src/components/MiniCard'
 import {faLock, faPeopleGroup, faCalendar, faFunnelDollar, faBell, faSquareH, faCirclePlay} from "@fortawesome/free-solid-svg-icons"
 
