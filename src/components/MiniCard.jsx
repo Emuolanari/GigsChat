c51d5eb4948879d4cd9574ef39e1057bf79501de
nariogulu <emuolanari.osah-ogulu@northlink.digital.com>
2022-07-10 13:29:07 +0100

    white border mini cards

--- src/components/MiniCard.jsx
+++ src/components/MiniCard.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function MiniCard({title, body, icon, iconColor}) {
   return (
-    <div className='card shadow p-3 mb-3 simple-linear position-relative' style={{ width:'13.2rem', minWidth: '13rem', height:'15rem', marginRight:20, borderRadius:18 }}>
+    <div className='card shadow p-3 mb-3 simple-linear position-relative' style={{ width:'13.2rem', minWidth: '13rem', height:'15rem', marginRight:20, borderRadius:18, border:'3px solid white' }}>
             <FontAwesomeIcon icon={icon} style={{position:'absolute', top:-9, bottom:0, zIndex:200}} color={iconColor}/>
         <div className="card-body text-center">
             <div className='bg-info.bg-gradient'>
