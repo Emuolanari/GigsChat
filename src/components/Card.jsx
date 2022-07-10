6234b67df1152c8935f07fc1a2d4e9246bc62862
nariogulu <emuolanari.osah-ogulu@northlink.digital.com>
2022-07-10 01:09:06 +0100

    added heart and settings icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,21 @@
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import {faEllipsisV, faHeart} from "@fortawesome/free-solid-svg-icons"
 
 export default function Card({name, story, imgSrc, about}) {
   return (
     <div className='col'>
       <div className="card shadow p-4 mb-3" style={{ minWidth: '16rem', minHeight:'28rem', borderRadius:15 }}>
         <div className="card-body text-center justify-content-center">
-          <div className='mx-auto'>
-            <Image src={imgSrc} alt="thumbnail" className='rounded-circle img-thumbnail' style={{borderRadius:'50%'}} height={112} width={112}/>
+          <div className="d-flex">
+            <div className='mx-auto'>
+              <Image src={imgSrc} alt="thumbnail" className='rounded-circle img-thumbnail' style={{borderRadius:'50%'}}
+                height={112} width={112} />
+            </div>
+            <FontAwesomeIcon className="p-2" icon={faHeart} />
+            <FontAwesomeIcon className="p-2" icon={faEllipsisV} />
+
           </div>
           <h5 className="card-title d-flex flex-column mb-4">{name}<span style={{fontSize:13}} className='text-muted'>{about}</span></h5>
           
