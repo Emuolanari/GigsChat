d9ff95cb9763aa41cb067ae54d07733446a48c72
nariogulu <emuolanari.osah-ogulu@northlink.digital.com>
2022-07-12 22:46:34 +0100

    button font

--- src/components/WaitingListBtn.jsx
+++ src/components/WaitingListBtn.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function WaitingListBtn({passedColor}) {
   return (
-    <button className="rounded text-white border-0" style={{backgroundColor: `${passedColor}`, padding:12}}><span>Join the waitlist &nbsp;
+    <button className="rounded text-white border-0" style={{fontFamily:'Raleway', fontWeight:'400', backgroundColor: `${passedColor}`, padding:12}}><span>Join the waitlist &nbsp;
         <FontAwesomeIcon icon ={faArrowRight}/></span>
       </button>
   )
