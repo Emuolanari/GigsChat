05af3453c4092f4c777cf011bd72f66dbcd9d1a5
nariogulu <emuolanari.osah-ogulu@northlink.digital.com>
2022-07-14 17:19:53 +0100

    better image size

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -5,10 +5,10 @@ const Message = ({name, message}) => {
   return (
     <div className="d-flex rounded align-items-center shadow-sm" style={{ backgroundColor:'rgba(255,255,255,0.9)', maxWidth:200, height: 80, fontSize:10, padding:4 }}>
         <div className="d-flex justify-content-between" style={{width:'100%'}}>
-            <div className="p-1">
+            <div className="p-1" style={{width:'30%'}}>
                 <Image src={NigerGirl} alt='message' objectFit="cover" height={55} width={55} style={{borderRadius:'50%'}}/>
             </div>
-            <div className="d-flex flex-column p-1">
+            <div className="d-flex flex-column p-1" style={{width:'70%'}}>
                 <span style={{fontFamily:'Raleway', fontWeight:600}}>{name}</span>
                 <span style={{fontFamily:'Raleway', fontWeight:200}}>{message}</span>
             </div>
