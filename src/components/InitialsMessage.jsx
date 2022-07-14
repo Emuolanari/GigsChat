a3fe5e42068f028edd10235d43a10dee1a25e5fa
nariogulu <emuolanari.osah-ogulu@northlink.digital.com>
2022-07-14 19:48:37 +0100

    space in initials message

--- src/components/InitialsMessage.jsx
+++ src/components/InitialsMessage.jsx
@@ -4,7 +4,7 @@ const InitialsMessage = ({message, initials, color}) => {
         <div className="d-flex justify-content-center align-items-center gap-2" style={{ borderRadius:'50%', backgroundColor:color, height:30, width:30 }}>
             <span className="text-white" style={{fontFamily:'Raleway', fontWeight:400}}>{initials}</span>
         </div>
-        <div className="d-flex align-items-center shadow-sm p-1" style={{width:'90%', borderRadius:10, backgroundColor:'rgba(255,255,255,0.9)'}}>
+        <div className="d-flex align-items-center shadow-sm p-1" style={{width:'85%', borderRadius:10, backgroundColor:'rgba(255,255,255,0.9)'}}>
             <span className="text-muted" style={{fontFamily:'Raleway'}}>
                 {message}
             </span>
