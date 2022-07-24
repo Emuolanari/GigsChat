33c437f7458982a72f8b0af9349213faf70feead
Emuolanari Ogulu <emuolanariogulu@Emuolanaris-MacBook-Air.local>
2022-07-24 14:37:17 +0100

    handling form changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FormContext } from '../../pages/_app';
@@ -7,7 +7,15 @@ import { FormContext } from '../../pages/_app';
 // import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export default function FormModal() {
-    const {isFormVisible, setIsFormVisible} = useContext(FormContext); 
+  const {isFormVisible, setIsFormVisible} = useContext(FormContext); 
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [intendedUse, setIntendedUse] = useState('')
+  const [socialHandle, setSocialHandle] = useState('')
+
+  const handleSubmit = () => {
+    console.log('intendedUse', intendedUse);
+  }
   return (
     <Modal show={isFormVisible} onHide={()=>setIsFormVisible(false)}>
       <Modal.Header closeButton>
@@ -17,32 +25,32 @@ export default function FormModal() {
       <Modal.Body>
         <div className="mb-5">
         {/* <FontAwesomeIcon className="p-2 position-absolute" icon={faPerson}/> */}
-          <input type="text" className="form-control" id='form-name'/>
+          <input type="text" className="form-control" id='form-name' onChange={(e)=>setName(e.target.value)} required/>
           <label data-error="wrong" data-success="right" htmlFor="form-name">Your Name</label>
         </div>
         <div className="mb-5">
         {/* <FontAwesomeIcon className="p-2 position-absolute" icon={faEnvelope}/> */}
-          <input type="email" className="form-control input-lg" id='form-email'/>
+          <input type="email" className="form-control input-lg" id='form-email' onChange={(e)=>setEmail(e.target.value)} required/>
           <label data-error="wrong" data-success="right" htmlFor="form-email">Your email</label>
         </div>
         <div className="mb-5">
-          <select className="form-select" id='form-select'>
+          <select className="form-select" id='form-select' defaultValue={'Client'} onChange={(e)=>setIntendedUse(e.target.value)} required>
             {/* <option selected>Open this select menu</option> */}
-            <option selected value="Client">Client</option>
+            <option value="Client">Client</option>
             <option value="Creator">Creator</option>
           </select>
           <label data-error="wrong" data-success="right" htmlFor="form-select">Intended Use</label>
         </div>
         <div className="mb-5">
         {/* <FontAwesomeIcon className="p-2 position-absolute" icon={faInstagram}/> */}
-          <input type="text" className="form-control input-lg" id="form-socials"/>
+          <input type="text" className="form-control input-lg" id="form-socials" onChange={(e)=>setSocialHandle(e.target.value)}/>
           <label data-error="wrong" data-success="right" htmlFor="form-socials">Your Social Media @</label>
         </div>
       </Modal.Body>
 
       <Modal.Footer>
         <Button variant="secondary" onClick={()=>setIsFormVisible(false)}>Close</Button>
-        <Button style={{backgroundColor:'#0bc5b9'}}>Submit</Button>
+        <Button style={{backgroundColor:'#0bc5b9'}} onClick={handleSubmit}>Submit</Button>
       </Modal.Footer>
     </Modal>
   );
