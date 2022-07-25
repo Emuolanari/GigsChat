1dbe4242617d6fd291ddcb470653e5f3d651f5fb
Emuolanari Ogulu <emuolanariogulu@Emuolanaris-MacBook-Air.local>
2022-07-25 23:31:58 +0100

    react-spring

--- src/components/SecondAnimation.jsx
+++ src/components/SecondAnimation.jsx
@@ -5,12 +5,16 @@ import CoolGirl from './../assets/coolgirl.png'
 import Richard from './../assets/richard.jpg'
 import Iphone from './../assets/iphone.svg'
 import Iphone2 from './../assets/iphone2.svg'
+import { useTransition, animated } from "@react-spring/web";
 
 function SecondAnimation() {
-    //check https://www.npmjs.com/package/react-spring for animating cards
-    //https://www.youtube.com/watch?v=kT6yYSwK1oA&ab_channel=BiteSizeAcademy
-
+    
     const [timeToChange, setTimeToChange] = useState(2);
+    const transition = useTransition(timeToChange, {
+        from:{right:-190,y:150, opacity:0}, //right, top
+        enter:{x:120,bottom:170, opacity:1},//left, bottom
+        leave:{left:120,bottom:170, opacity:0}
+    })
     useEffect(() => {
         const interval = setInterval(() => setTimeToChange(!timeToChange), 1500)
     
@@ -22,28 +26,33 @@ function SecondAnimation() {
   return (
     <div className="d-flex justify-content-center text-white">
         <div style={{maxWidth:'100%',maxHeight:'100%', height: 'auto', position:'relative'}}>
-            <div className='position-absolute' style={{zIndex:4, left:-120, bottom:170}}>
+            <div className="position-absolute" style={{zIndex:10}}>{transition((style, item)=>item?'':<animated.div style={style}>
+                <Message name='Jenny Wilson' message='No more messages from unserious clients' imgSrc={CoolGirl}
+                    color={'#FFAC0E'} maxW={250} padding={9} />
+            </animated.div>)}</div>
+            <div className='position-absolute' style={{zIndex:4,left:-120, bottom:170}}>
                 {timeToChange?
                 <Message name='Jenny Wilson' message='No more messages from unserious clients' imgSrc={CoolGirl}
                     color={'#FFAC0E'} maxW={250} padding={9} />:
                 <Message name='Richard Tokunbo' message='All my project milestones were easy to set track and review'
                     imgSrc={Richard} color={'#0BC5B9'} maxW={250} padding={9} />}
+        
             </div>
             {timeToChange?<div className='position-absolute' style={{zIndex:4, right:-130, top:150}}>
                 <Message name='Richard Tokunbo' message='All my project milestones were easy to set track and review'
                     imgSrc={Richard} color={'#0BC5B9'} maxW={250} padding={9} />
             </div>:
             (<>
-                <div className='position-absolute' style={{zIndex:4, right:-130, top:150}}>
+                <div className='position-absolute' style={{zIndex:40, right:-130, top:150}}>
                     <Message name='Richard Tokunbo'
                         message='All my project milestones were easy to set track and review' imgSrc={Richard}
                         color={'#0BC5B9'} maxW={250} padding={9} />
                 </div>
 
-                <div className='position-absolute' style={{zIndex:1, right:-110, top:150}}>
+                {/* <div className='position-absolute' style={{zIndex:1, right:-110, top:150}}>
                     <Message name='Jenny Wilson' message='No more messages from unserious clients' color={'#FFAC0E'}
                         maxW={250} padding={9} />
-                </div>
+                </div> */}
             </>
             )
             }
